f9e4979f88bbacd1624e9eb0d3287e30a00ffa00
hongjunpyo11 <jpyo98@naver.com>
2022-08-22 13:30:41 +0900

    질문 작성시에 현재 로그인한 회원을 작성자로 지정

--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -2,12 +2,13 @@ package com.ll.exam.sbb.question;
 
 import com.ll.exam.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
+import com.ll.exam.sbb.user.SiteUser;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,10 +32,11 @@ public class QuestionService {
                 .orElseThrow(() -> new DataNotFoundException("no %d question not found,".formatted(id)));
     }
 
-    public void create(String subject, String content) {
+    public void create(String subject, String content, SiteUser author) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
+        q.setAuthor(author);
         q.setCreateDate(LocalDateTime.now());
         questionRepository.save(q);
     }
